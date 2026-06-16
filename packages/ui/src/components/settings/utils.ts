75915b7be6e4e62b535675e75fa4e9dd6e50757b
JW <jiangweixian1994@gmail.com>
2026-06-16 13:55:17 +0800

    refactor(ui): remove unused component inventory

--- packages/ui/src/components/settings/utils.ts
+++
@@ -1,6 +0,0 @@
-// Lightweight class-name joiner for settings sub-components.
-// Filters falsy values so conditional classes can be passed without ternaries.
-
-export function cn(...classes: (string | false | null | undefined)[]) {
-  return classes.filter(Boolean).join(' ')
-}
--- packages/ui/src/components/ui/Toast.tsx
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line react-refresh/only-export-components
-export { toast, Toaster } from 'sonner'
